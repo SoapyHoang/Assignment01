3b71a8bb8759e6f82a99843d82ad258141cfd5c6
SoapyHoang <sxhoang@ucsd.edu>
2022-02-22 00:52:55 -0800

    completed assignment

--- js/script.js
+++ js/script.js
@@ -1,2 +1,132 @@
-// this is a test. 
-// another test
+// STEP 1
+// let someMonth; 
+// function theMonth();
+// let currentMonth; 
+// let summerMonth; 
+// let myLibraryFunction
+
+// STEP 2
+// string literal 
+// console.log('I have the high ground!');
+// Boolean literal 
+// console.log(confirm('Do you want to continue?'));
+// numeric literal 
+// console.log(3); 
+// null literal 
+// console.log(null);
+
+// STEP 3
+// let myEquation = 100 * (4/2) + 5;
+// console.log(prompt(myEquation));
+
+// let newNerdle = 2 * 7 - 3;
+// console.log(prompt('The correct answer is', newNerdle));
+
+// STEP 4
+// let FirstName; 
+// let LastName; 
+// let myAddress;
+// let myCity; 
+// let myState; 
+// let ZipCode; 
+// let yourAge; 
+// let ReferralSource; 
+// let MayWeContactYou; 
+
+// STEP 5
+// let FirstName; 
+// let LastName; 
+// let myAddress;
+
+// FirstName = 'Sophia';
+// LastName = 'Hoang';
+// myAddress = '1234 Sesame Street';
+// document.write(FirstName, ' ', LastName, '\n', myAddress);
+
+// let FirstName = 'Sophia';
+// let LastName = 'Hoang';
+// let myAddress = '1234 Sesame Street';
+// document.write(FirstName, ' ', LastName, '\n', myAddress);
+
+// let FirstName = 'Sophia', LastName = 'Hoang', myAddress = '1234 Sesame Street';
+// document.write(FirstName, ' ', LastName, '\n', myAddress);
+
+// STEP 6
+// let myExample = 2022
+// console.log('I keep writing the year ' + myExample + ' in my assignments.')
+
+// STEP 7
+// let firstVariable = true + " this is string"
+// console.log(firstVariable);
+
+// let x = 10 + true; 
+// console.log(x);
+
+// STEP 8
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former"\.';
+// window.console.log(someString);
+
+// STEP 9
+// let myExample = null; 
+// console.log(myExample);
+
+// let var2
+// console.log(var2);
+
+// STEP 10
+// console.log(typeof 'Hello')
+// console.log(typeof 5)
+// console.log(typeof true) 
+// console.log(typeof ['Hello', 'It\'s', 'Me'])
+// console.log(typeof undefined)
+
+// STEP 11
+// alert('Hello' + ' Sophia Hoang' + ', welcome to the JavaScript class!')
+
+// STEP 12
+// let myName = 'Sophia Hoang'
+// alert('Hello ' + myName + ' , welcome to the JavaScript class!')
+
+// STEP 13
+// let myName = 'Sophia Hoang'
+// let course = 'JavaScript'
+// alert('Hello ' + myName + ' , welcome to the ' + course + ' class!')
+
+// STEP 14
+// alert('Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!')
+
+// STEP 15
+// let myName = prompt("What is your name?")
+// let course = 'JavaScript'
+// alert('Hello ' + myName + ' , welcome to the ' + course + ' class!')
+
+// STEP 16
+// let myName = prompt("What is your name?")
+// let course = prompt('What class are you taking?')
+// alert('Hello ' + myName + ' , welcome to the ' + course + ' class!')
+
+// STEP 17
+// let x = 10
+// let y = 20
+// console.log(x + y)
+
+// STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+
+// STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// STEP 20
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// STEP 21
+// console.log( 5 == 5 || 5 != 5)
+
+// STEP 22
+// console.log(5 > 6 && 5 < 6)
